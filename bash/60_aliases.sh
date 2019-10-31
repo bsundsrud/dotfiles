ac36198c61fc874281d389050eb52472c25440b2
Benn Sundsrud <benn.sundsrud@gmail.com>
2019-10-31 11:46:08 +0100

    Add ky alias

--- bash/60_aliases.sh
+++ bash/60_aliases.sh
@@ -1,5 +1,6 @@
 alias mem_usage="smem -kts uss"
 alias kc="kubectl"
 alias kce="kubectl get events --sort-by='{.lastTimestamp}'"
+alias ky="kubectl -oyaml"
 alias durl='curl -sSD- -o /dev/null -H "X-debug: true"'
 alias hurl='curl -sSD- -o /dev/null'
